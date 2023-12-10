5ea9c1f3aac272d7207e87918d595c3b519be336
Neeraj Bhatt <neeraj1bh@gmail.com>
2023-12-10 11:28:16 +0530

    chore: added navbar and themes

--- app/containers/Playground.tsx
+++ app/containers/Playground.tsx
@@ -1,10 +1,12 @@
 import PlaygroundMessages from './MessageComponents/PlaygroundMessages';
 import SystemMessage from './MessageComponents/SystemMessage';
+import Navbar from './Navbar';
 import Sidebar from './MessageComponents/Sidebar';
 
 const Playground = () => {
   return (
     <div className="flex flex-col min-h-screen">
+      <Navbar />
       <div className="flex py-6 h-full flex-wrap px-12 gap-6">
         <div className="flex flex-col gap-4 grow">
           <SystemMessage />
